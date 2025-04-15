2a25e7fd6c5d72f66059cb5a45fea3bdbb7f7c84
ndm7035 <jainsanket7035@gmail.com>
2025-04-16 01:06:53 +0530

    Update and rename app.jsx to src/app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,6 @@
+npm create vite@latest new-digital-media --template react
+cd new-digital-media
+npm install
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
